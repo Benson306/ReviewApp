8c131ca87eab35e83225ac8dfc0436bc13401bf5
Benson Ndiwa <bnkimtai@gmail.com>
2023-03-11 11:32:22 +0300

    Global Styles

--- screens/about.js
+++ screens/about.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { StyleSheet,Text, View } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function Abiut(){
     return ( 
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>About Screen</Text>
         </View>
      );
 }
 
-const styles = StyleSheet.create({
-    container:{
-        padding:24
-    }
-})
--- screens/home.js
+++ screens/home.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { StyleSheet,Text, View } from "react-native";
 import * as Font from 'expo-font'
-
+import { globalStyles } from "../styles/global";
 export default function Home(){
     return ( 
-        <View style={styles.container}>
-            <Text style={styles.titleText}>Home Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Home Screen</Text>
         </View>
      );
 }
 
-const styles = StyleSheet.create({
-    container:{
-        padding:40
-    },
-    titleText:{
-        fontFamily: 'sano-bold',
-        fontSize:18
-    }
-})

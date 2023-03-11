64adb196b8a17d0dce95a91e1dfd14ac787b0964
Benson Ndiwa <bnkimtai@gmail.com>
2023-03-11 23:18:39 +0300

    Forms with formik

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import { Button, FlatList, StyleSheet,Text, TouchableOpacity, View } from "react-native";
+import { Button, FlatList, Keyboard, Modal, StyleSheet,Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import * as Font from 'expo-font'
 import { globalStyles } from "../styles/global";
 import Card from "../shared/Card";
+import { MaterialIcons } from '@expo/vector-icons'
+import ReviewForm from "./ReviewForm";
+
+
 export default function Home({ navigation }){
     
     // const pressHandler = ()=>{
@@ -15,10 +19,43 @@ export default function Home({ navigation }){
         {title:'Forest Gumpp',rating: 5,body: 'lorem ipsum', key: '2'},
         {title:'American Made',rating: 4,body: 'lorem ipsum', key: '3'}
     ])
+
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const addReview = (reviews)=>{
+        reviews.key = Math.random().toString();
+        setReviews((currentReviews)=>{
+            return [reviews,...currentReviews];
+        })
+
+        setModalOpen(false);
+    }
+
     return ( 
         <View style={globalStyles.container}>
             {/* <Text style={globalStyles.titleText}>Home Screen</Text> */}
             {/* <Button title='go to review details' onPress={pressHandler} /> */}
+            <Modal visible={modalOpen} animationType='slide'>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.modalContent}>
+                        <MaterialIcons 
+                            name="close" 
+                            size={24} 
+                            style={{...styles.modalToggle, ...styles.modalClose}}
+                            onPress={()=> setModalOpen(false)}
+                        />
+                        <ReviewForm addReview={addReview} />
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+ 
+            <MaterialIcons 
+                name="add" 
+                size={24} 
+                style={styles.modalToggle}
+                onPress={()=> setModalOpen(true)}
+            />
+
             <FlatList 
                 data={reviews}
                 renderItem={({ item }) =>(
@@ -33,3 +70,21 @@ export default function Home({ navigation }){
      );
 }
 
+const styles = StyleSheet.create({
+    modalToggle:{
+        marginBottom: 10,
+        borderWidth:1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 1,
+        alignSelf:'center'
+    },
+    modalClose:{
+        marginTop: 20,
+        marginBottom: 0
+    },
+    modalContent:{
+        flex:1,
+        padding:10
+    }
+})
---
+++ screens/ReviewForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
+import { Formik } from 'formik'
+
+export default function ReviewForm({ addReview }){
+    return (
+        <View style={globalStyles.container}>
+            <Formik
+                initialValues={{title: '', body: '', rating: ''}}
+                onSubmit={(values, actions)=>{
+                    addReview(values);
+                    actions.resetForm();
+                }}
+            >
+                {(props)=>(
+                    <View>
+                        <TextInput 
+                            style={globalStyles.input}
+                            placeholder="Review Title"
+                            onChangeText={props.handleChange('title')}
+                            value={props.values.title}
+                        />
+                        <TextInput 
+                            multiline
+                            style={globalStyles.input}
+                            placeholder="Review Body"
+                            onChangeText={props.handleChange('body')}
+                            value={props.values.body}
+                        />
+                        <TextInput 
+                            style={globalStyles.input}
+                            placeholder="Rating (1-5)"
+                            onChangeText={props.handleChange('rating')}
+                            value={props.values.rating}
+                            keyboardType='numeric'
+                        />
+                        <Button title='submit' color='maroon' onPress={props.handleSubmit} />
+                    </View>
+                )}
+            </Formik>
+        </View>
+    )
+}

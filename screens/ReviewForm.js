c6fc5dfff76bfaf1a697b906375bedbf4b0e0dca
Benson Ndiwa <bnkimtai@gmail.com>
2023-03-11 23:40:49 +0300

    Form validation with yup

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 import { globalStyles } from "../styles/global";
-import { Formik } from 'formik'
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const ReviewSchema = yup.object({
+    title: yup.string()
+            .required()
+            .min(4),
+    body: yup.string()
+            .required()
+            .min(5),
+    rating: yup.string()
+            .required()
+            .test('is-num-1-5','Rating must be a number 1 - 5', (val)=>{
+                return parseInt(val) < 6 && parseInt(val) > 0;
+            })
+
+})
 
 export default function ReviewForm({ addReview }){
     return (
         <View style={globalStyles.container}>
             <Formik
                 initialValues={{title: '', body: '', rating: ''}}
+                validationSchema={ReviewSchema}
                 onSubmit={(values, actions)=>{
                     addReview(values);
                     actions.resetForm();
@@ -20,21 +37,27 @@ export default function ReviewForm({ addReview }){
                             placeholder="Review Title"
                             onChangeText={props.handleChange('title')}
                             value={props.values.title}
+                            onBlur={props.handleBlur('title')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
                         <TextInput 
                             multiline
                             style={globalStyles.input}
                             placeholder="Review Body"
                             onChangeText={props.handleChange('body')}
                             value={props.values.body}
+                            onBlur={props.handleBlur('body')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
                         <TextInput 
                             style={globalStyles.input}
                             placeholder="Rating (1-5)"
                             onChangeText={props.handleChange('rating')}
                             value={props.values.rating}
                             keyboardType='numeric'
+                            onBlur={props.handleBlur('rating')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.rating &&props.errors.rating}</Text>
                         <Button title='submit' color='maroon' onPress={props.handleSubmit} />
                     </View>
                 )}
